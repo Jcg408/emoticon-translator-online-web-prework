e0559910cc4d9a55876bce390dbc975090469ff2
Jessica Gladstone <jcg321@comcast.net>
2018-10-20 20:10:10 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ library = YAML.load_file(filepath)
 hash = {"get_meaning"=>{}, "get_emoticon"=>{}}
 library.each do |k,v|
   hash["get_meaning"][value[1]] = key
-  
+  hash["get_emoticon"][value[0]] = value[1]
  end
 end
 
