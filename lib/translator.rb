ab371657f8efe86637683d0dbfa783894a622836
Jessica Gladstone <jcg321@comcast.net>
2018-10-20 20:08:03 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,9 @@ require "yaml"
 def load_library(filepath)
 library = YAML.load_file(filepath)
 hash = {"get_meaning"=>{}, "get_emoticon"=>{}}
-# library.each do |k,v|
-#   hash["get_meaning"][value[1]]=key
- 
-# end
+library.each do |k,v|
+  hash["get_meaning"][value[1]] = key
+ end
 end
 
 def get_japanese_emoticon
