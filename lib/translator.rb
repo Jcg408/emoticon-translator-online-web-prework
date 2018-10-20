ce79a596fe620e338f3c68e381c85b3a18596173
Jessica Gladstone <jcg321@comcast.net>
2018-10-20 20:06:21 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ require "yaml"
 def load_library(filepath)
 library = YAML.load_file(filepath)
 hash = {"get_meaning"=>{}, "get_emoticon"=>{}}
-library.each do |k,v|
-  hash["get_meaning"][value[1]]=key
+# library.each do |k,v|
+#   hash["get_meaning"][value[1]]=key
  
 end
 end
